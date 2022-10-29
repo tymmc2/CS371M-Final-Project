dfe94be8cbc5094a1c44971400ea15c50244be0c
RamitPahwa <ramitpahwa123@gmail.com>
2022-10-28 21:55:25 -0500

    Bottom Navigation Bar

--- StockApp/app/src/main/java/com/example/stockapp/home/HomeActivity.kt
+++ StockApp/app/src/main/java/com/example/stockapp/home/HomeActivity.kt
@@ -3,9 +3,15 @@ package com.example.stockapp.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.stockapp.R
 import com.example.stockapp.stockcard.StockCardAdapter
 import com.example.stockapp.databinding.ActivityHomeBinding
 import com.example.stockapp.stockcard.StockCardViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -15,11 +21,19 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Load stocks
-        val adapter = StockCardAdapter(applicationContext)
-        binding.stocksRecyclerView.adapter = adapter
-        val stocksViewModel = ViewModelProvider(this)[StockCardViewModel::class.java]
-        stocksViewModel.init()
-        binding.stocksRecyclerView.setHasFixedSize(true)
+
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
